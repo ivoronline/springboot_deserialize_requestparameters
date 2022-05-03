bb5ed0e00becb442f354f985113b93a23726f0f1
igoreta <ivoronline@gmail.com>
2022-05-03 10:37:14 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_deserialize_requestparameters/DTO/PersonDTO.java
+++ src/main/java/com/ivoronline/springboot_deserialize_requestparameters/DTO/PersonDTO.java
@@ -7,7 +7,13 @@ public class PersonDTO {
   public Integer age;
 
   //SETTERS (Setter Names are HTTP Request Parameters)
-  public void setFirstName(String  reqName) { this.name = reqName + " Setter"; }
-  public void setAge      (Integer reqAge ) { this.age  = reqAge;              }
+  public void setFirstName(String  reqName) { this.name = this.name + reqName + " Setter"; }
+  public void setAge      (Integer reqAge ) { this.age  = reqAge;                          }
+
+  //CONSTRUCTOR
+  public PersonDTO(String firstName, Integer age) {
+    this.name = firstName + " Constructor ";
+    this.age  = age;
+  }
 
 }
